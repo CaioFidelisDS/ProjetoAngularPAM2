480ff38e0042d3024bf73dce9d31e64880ae3dba
CaioFidelisDS <125466050+CaioFidelisDS@users.noreply.github.com>
2024-09-09 10:47:16 +0000

    criação calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { AlertController, IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
+
 })
-export class HomePage {
-  constructor() {}
-}
+export class HomePage {  
+public gasolina: number | undefined;
+public alcool: number | undefined;
+public media: number | undefined;
+public situacao: string = "";
+constructor((private alertController: AlertControlle){}
+calcularMedia() {
+if (this.gasolina && this.alcool) {
+this.media = (this.alcool / this.gasolina);
+this.situacao = this.media <= 0.7 ? 'Use alcool' : 'Use Gasolina';
+      } else {
+        this.exibirAlerta();
+        }
+        }
+        async exibirAlerta() {
+        const alert = await this.alertController.create({
+        header: 'Atenção',
+        message: 'Por favor, digite as duas notas antes de calcular a média',
+        buttons: ['OK']
+        });
+        await alert.present();
+        }
+        }
+
+
+
